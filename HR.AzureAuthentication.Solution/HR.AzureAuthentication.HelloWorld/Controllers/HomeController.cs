0531862fb9fc3401fe2c6a1b7d3bf868f71a2933
debasishlenka90 <debasishlenka90@gmail.com>
2021-10-30 10:43:02 +0530

    add new project

---
+++ HR.AzureAuthentication.Solution/HR.AzureAuthentication.HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using HR.AzureAuthentication.HelloWorld.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace HR.AzureAuthentication.HelloWorld.Controllers
+{
+    //[Authorize]
+    
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+           //// Get the group list from the Azure AD.
+          //// var groups = User.Claims.Where(c => c.Type == "groups").ToList();
+          return View();
+        }
+       // [Authorize(Policy = "Users")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+       // [Authorize(Policy = "Admins")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HR.AzureAuthentication.Solution/README.md
@@ -0,0 +1,8 @@
+https://www.c-sharpcorner.com/article/authentication-and-authorization-in-asp-net-core-2-0-using-azure-active-director/
+https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-aspnet-core-webapp
+
+Configure authentication in a sample web app by using Azure AD B2C
+
+https://docs.microsoft.com/en-us/azure/active-directory-b2c/configure-authentication-sample-web-app?tabs=visual-studio#step-2-register-a-web-application
+
+https://docs.microsoft.com/en-us/azure/active-directory-b2c/enable-authentication-web-application?tabs=visual-studio
